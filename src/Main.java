2763288b6edf0070a15d90c107db84329edfa01c
Diarmuid <Diarmuid-O-Donnchadha@protonmail.com>
2021-10-18 21:54:03 +0100

    Okay but actually this time

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,70 @@
+import Board.Board;
+import Board.Move;
 import Board.Perft;
+import Board.MoveGenerator;
 import GameManager.Game;
 
 public class Main {
     public static void main(String[] args) {
         Perft perft = new Perft();
+        MoveGenerator moveGenerator = new MoveGenerator();
 
-        //For use in tandem with perftree, for debugging Move Generation
         if(args.length == 3) {
-            perft.runPerft(args[0], args[1], args[2]);
-            return;
+            int depth = Integer.parseInt(args[0]) - 1;
+            String FEN = args[1];
+            int start = getIndexFromSquare("" + args[2].charAt(0) + args[2].charAt(1));
+            int target = getIndexFromSquare("" + args[2].charAt(2) + args[2].charAt(3));
+
+            Board board = moveGenerator.makeMove(start, target, new Board(FEN));
+
+            long total = perft.perft(depth, board);
+
+            for (Move move : moveGenerator.getLegalMoves(board)) {
+                String startSq = squareFromIndex(move.startSquare);
+                String targetSq = squareFromIndex(move.targetSquare);
+
+                System.out.println(startSq + targetSq + " " + perft.perft(depth - 1, moveGenerator.makeMove(move.startSquare, move.targetSquare, board)));
+            }
+
+            System.out.println("\n" + total);
         } else if(args.length == 2) {
-            perft.runPerft(args[0], args[1]);
-            return;
+            int depth = Integer.parseInt(args[0]);
+            String FEN = args[1];
+
+            Board board = new Board(FEN);
+
+            long total = perft.perft(depth, board);
+
+            for(Move move : moveGenerator.getLegalMoves(board)) {
+                String startSq = squareFromIndex(move.startSquare);
+                String targetSq = squareFromIndex(move.targetSquare);
+
+                System.out.println(startSq + targetSq + " " + perft.perft(depth - 1, moveGenerator.makeMove(move.startSquare, move.targetSquare, board)));
+            }
+
+            System.out.println("\n" + total);
         }
+    }
 
-        Game chess = new Game("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
-        chess.printBoard();
-        System.out.println();
-        chess.printBoard(chess.moveGenerator.getAttackedSquares(chess.board, chess.board.whiteToMove));
-        //System.out.println("King in Check: " + chess.moveGenerator.kingInCheck(chess.board, chess.board.whiteToMove));
-        System.out.println();
+    public static char getFile(int index) {
+        return (char) ((index % 8) + 97);
+    }
 
-        System.out.println(chess.getLegalMoves() + "\n\nMoves: " + chess.getLegalMoves().size());
+    public static char getRank(int index) {
+        return (char) ((index / 8) + 49);
+    }
 
-        for (int i = 0; i < 6; i++) System.out.println("Moves at depth " + i + ": " + perft.perft(i, chess.board));
+    public static String squareFromIndex(int index) {
+        char number = getRank(index);
+        char letter = getFile(index);
+
+        return letter + String.valueOf(number);
+    }
+
+    public static int getIndexFromSquare(String square) {
+        int file = Integer.parseInt("" + ((square.charAt(0) - 97)) * 8) - 1;
+        int rank = Integer.parseInt("" + square.charAt(1));
+
+        return file + rank;
     }
 }
